9974d43d13895180302395812804e2a5065c2eb6
juan8liscano <jjliscano@estudiante.uniajc.edu.co>
2026-03-09 16:20:57 +0000

    Ejerccio4.12

---
+++ clase6/Ejerccio412.java
@@ -0,0 +1,44 @@
+package clase6;
+
+import java.util.Scanner;
+
+public class Ejerccio412 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double num1, num2;
+        int seleccion;
+        System.out.println("Digite numero1");
+        num1 = sc.nextDouble();
+        System.out.println("Digite numero2");
+        num2 = sc.nextDouble();
+        System.out.println("Digite lo seleccion 1,2.3");
+        seleccion = sc.nextInt();
+
+        if (seleccion == 1) {
+
+            System.out.println("la suma"  + (num1 + num2));
+        } else {
+            if (seleccion == 2) {
+
+                System.out.println("la multipica" + (num1 * num2));
+
+            } else {
+
+                if (seleccion == 3) {
+
+                    System.out.println("la division" + (num1 / num2));
+
+                } else {
+                    System.out.println("opcion no valida");
+
+                }
+            } // else
+
+        }
+
+        sc.close();
+
+    }
+}
